069fea4d6c01f9a685933f717a32a0a0509c136d
Elena Carey <theelenacarey@gmail.com>
2024-03-02 15:43:36 -0600

    Found it

--- app.js
+++ app.js
@@ -8,16 +8,40 @@ const passport = require('passport');
 const { ensureAuthenticated } = require("./validation/auth.js");
 require('./validation/auth.js')
 
+const app = express();
+const port = process.env.PORT || 3001;
+
 function isLoggedIn(req, res, next) {
     req.user ? next() : res.sendStatus(401)
 }
 
-const app = express();
-const port = process.env.PORT || 3001;
+// Configure session middleware
+app.use(session({
+    secret: process.env.SUPERSECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Initialize Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 
 app
-    .use(bodyParser.json());
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept'
+        );
+        res.setHeader(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+        );
+        next();
+    });
 
 
 
@@ -27,14 +51,23 @@ app.get("/", (req, res) => {
 app.get('/auth/google',
     passport.authenticate('google', {scope: ['email', 'profile'] }));
     
-app.get('auth/google/callback',
+app.get('/auth/google/callback',
     passport.authenticate('google', {
         successRedirect: '/protected',
         failureRedirect: '/auth/failure'
         }));
-app.use('/protected', (req, res) => {
-    res.send("hello")
-});
+app.use('/protected', isLoggedIn, require('./routes'));
+
+app.get('/logout', function(req, res, next){
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+  });
+
+app.get('/auth/google/failure', (req, res) => {
+    res.send('Failed to authenticate..');
+  });
 
 
 mongodb.initDb((err) => {
--- validation/auth.js
+++ validation/auth.js
@@ -1,21 +1,22 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const uri = "https://localhost:3001/auth/google/callback" || "https://horse-forge.onrender.com/auth/google/callback"
+const uri = "" || "https://horse-forge.onrender.com/auth/google/callback"
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: uri,
+    callbackURL: 'http://localhost:3001/auth/google/callback',
     passReqToCallback: true
 },
     function (request, accessToken, refreshToken, profile, cb) {
-
+        console.log("Inside Passport creation.")
         return cb(null, profile);
     }
 ));
 
 passport.serializeUser(function (user, cb) {
+    console.log("Inside serializer.")
     cb(null, user)
 })
 passport.deserializeUser(function (user, cb) {
